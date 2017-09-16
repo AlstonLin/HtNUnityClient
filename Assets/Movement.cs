9c295159b23298259981eea636eaa670c7549397
Charlie Zhang <charlie.zhang@shopify.com>
2017-09-16 13:58:32 -0400

    movement works

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -12,7 +12,7 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0 || Input.GetMouseButton(0)) {
-			
+			transform.Translate (Camera.main.transform.forward * 0.1f);
 		}
 	}
 }
